1ef011b83129b5f54595572ba0c0a116021ca0c5
Jack Fan <jackrf2015@outlook.com>
2024-07-30 10:55:57 +0800

    v0.0.1
    v0.0.1 - first working version

--- local-tester/tools.py
+++ local-tester/tools.py
@@ -1,32 +1,35 @@
-import time
 import subprocess
+import os
+import sys
+import requests
 
-def get_results(test_result, raw_result):
-    test_result["instances"] = raw_result[0]
-    test_result["solutions"] = raw_result[1]
-    test_result["invalid_solutions"] = raw_result[0] - (raw_result[1] + raw_result[2])
-    test_result["errors"] = raw_result[2]
-    return test_result
-
-def loop_entry(algorithm_name, raw_result, timer):
-    worker_result = [0,0]
-    t_end = time.time() + timer
-    while time.time() < t_end:
-        raw_result[0] += 1
-        run_result = run_test(algorithm_name, raw_result[0])
-        if run_result == 0:
-            worker_result[0] += 1
-        elif run_result == 2:
-            worker_result[1] += 1
-
-    raw_result[1] += worker_result[0]
-    raw_result[2] += worker_result[1]
-
-def run_test(algorithm_name, i):
-    result = subprocess.run(f"../target/release/tig-worker -- settings.json wasm/{algorithm_name}.wasm {i}", shell=True, capture_output=True, text=True)
+def run_test(i):
+    result = subprocess.run(f"../target/release/tig-worker -- settings.json wasm/{os.environ["algorithm_name"]}.wasm {i}", shell=True, capture_output=True, text=True)
     return result.returncode
 
 def build_worker():
     _ = subprocess.run(f"cargo build -p tig-worker --release", shell=True, capture_output=True, text=True)
     print(f"tig-worker successfully built")
     
+def check_algorithm_existence(algorithm_name, algorithm_id_dict):
+    if os.path.exists(f"./wasm/{algorithm_name}.wasm") or algorithm_name in algorithm_id_dict.values():
+        return True
+    else:
+        return False
+    
+def dump_difficulty(difficulty):
+    if difficulty[0].isnumeric() and difficulty[1].isnumeric():
+        difficulty = [int(x) for x in difficulty]
+        with open("settings.json", "w+") as file:
+            settings = '{' + f'"block_id": "","algorithm_id": "","challenge_id": "","player_id": "","difficulty": {difficulty}' + '}'
+            file.write(settings)
+    else:
+        sys.exit("Your difficulty parameters entered are not integers, please re enter them and try again")
+
+def get_wasm_blob(algorithm_name, challenge_name):
+    branch_name = f"{challenge_name}/{algorithm_name}"
+    url = f"https://raw.githubusercontent.com/tig-foundation/tig-monorepo/{branch_name}/tig-algorithms/wasm/{branch_name}.wasm"
+    response = requests.get(url)
+    with open(f"wasm/{algorithm_name}.wasm", "wb") as file:
+        file.write(response.content)
+    print(f"Downloaded {branch_name}")
