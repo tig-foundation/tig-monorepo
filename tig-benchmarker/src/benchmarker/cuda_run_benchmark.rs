fa4e28dc63a98c581f3ddae23ee776abc19d6542
FiveMovesAhead <ying@tig.foundation>
2024-08-28 19:43:01 +0800

    Player 0x585bc8424694d541a1d5d6c293d8f7efe377b2a6 submitted 'inbound' for challenge satisfiability

--- tig-benchmarker/src/benchmarker/cuda_run_benchmark.rs
+++ tig-benchmarker/src/benchmarker/cuda_run_benchmark.rs
@@ -147,8 +147,8 @@ pub async fn execute(
                                     // #[cfg(feature = "c001_a017")]
                                     // "c001_a017" => Some((c001::c001_a017::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a017::KERNEL)),
 
-                                    // #[cfg(feature = "c001_a018")]
-                                    // "c001_a018" => Some((c001::c001_a018::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a018::KERNEL)),
+                                    #[cfg(feature = "c001_a018")]
+                                    "c001_a018" => Some((c001::c001_a018::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a018::KERNEL)),
 
                                     // #[cfg(feature = "c001_a019")]
                                     // "c001_a019" => Some((c001::c001_a019::cuda_solve_challenge as CudaSolveChallengeFn, &c001::c001_a019::KERNEL)),
