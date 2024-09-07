6b4a22d2e10d0d306ad5f8d4a665de9aab8bd355
FiveMovesAhead <ying@tig.foundation>
2024-09-08 06:14:20 +0800

    Fix bug where proof_submitter sometimes crashes.

--- tig-benchmarker/src/benchmarker/submit_proof.rs
+++ tig-benchmarker/src/benchmarker/submit_proof.rs
@@ -1,17 +1,24 @@
+use std::collections::HashSet;
+
 use super::{api, Job, Result};
 use tig_api::SubmitProofReq;
 
 pub async fn execute(job: &Job) -> Result<()> {
     let req = {
         let solutions_data = job.solutions_data.lock().await;
+        let sampled_nonces: HashSet<u64> = job
+            .sampled_nonces
+            .as_ref()
+            .expect("Expected sampled nonces")
+            .iter()
+            .cloned()
+            .collect();
         SubmitProofReq {
             benchmark_id: job.benchmark_id.clone(),
-            solutions_data: job
-                .sampled_nonces
-                .as_ref()
-                .expect("Expected sampled nonces")
+            solutions_data: solutions_data
                 .iter()
-                .map(|n| solutions_data[n].clone())
+                .filter(|(n, _)| sampled_nonces.contains(n))
+                .map(|(_, s)| s.clone())
                 .collect(),
         }
     };
