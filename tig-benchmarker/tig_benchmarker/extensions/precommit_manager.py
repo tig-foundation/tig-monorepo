c61535fd001a54b1768d80db4e0cf63cb73769c7
FiveMovesAhead <ying@tig.foundation>
2024-10-02 13:43:48 +0100

    Simple adaptive precommit manager.

--- tig-benchmarker/tig_benchmarker/extensions/precommit_manager.py
+++ tig-benchmarker/tig_benchmarker/extensions/precommit_manager.py
@@ -14,13 +14,16 @@ logger = logging.getLogger(os.path.splitext(os.path.basename(__file__))[0])
 @dataclass
 class AlgorithmSelectionConfig(FromDict):
     algorithm: str
-    num_nonces: int
     base_fee_limit: PreciseNumber
     weight: float
+    initial_num_nonces: int
+    max_num_nonces: int
+    target_duration: int
 
 @dataclass
 class PrecommitManagerConfig(FromDict):
     max_precommits_per_block: int
+    max_unresolved_precommits: int
     algo_selection: Dict[str, AlgorithmSelectionConfig]
 
 class Extension:
@@ -29,16 +32,28 @@ class Extension:
         self.config = PrecommitManagerConfig.from_dict(precommit_manager)
         self.num_precommits = 0
         self.last_block_id = None
+        self.num_unresolved_precommits = None
         self.curr_base_fees = {}
         self.difficulty_samples = {}
         self.algorithm_name_2_id = {}
         self.challenge_name_2_id = {}
+        self.performance_data = {}
         self.lock = True
 
-    async def on_new_block(self, block: Block, challenges: Dict[str, Challenge], algorithms: Dict[str, Algorithm], **kwargs):
-        if self.last_block_id != block.id:
-            self.num_precommits_submitted = 0
-            self.last_block_id = block.id
+    async def on_new_block(
+        self, 
+        block: Block, 
+        challenges: Dict[str, Challenge], 
+        algorithms: Dict[str, Algorithm], 
+        precommits: Dict[str, Precommit],
+        proofs: Dict[str, Proof],
+        **kwargs
+    ):
+        if self.last_block_id == block.id:
+            return
+        self.num_precommits_submitted = 0
+        self.num_unresolved_precommits = sum(1 for benchmark_id in precommits if benchmark_id not in proofs)
+        self.last_block_id = block.id
         for c in challenges.values():
             c_name = c.details.name
             assert c_name in self.config.algo_selection, f"missing algorithm selection for challenge {c_name}"
@@ -58,12 +73,39 @@ class Extension:
         logger.info(f"current base fees: {self.curr_base_fees}")
         self.lock = False
 
+    async def on_precommit_confirmed(self, precommit: Precommit, **kwargs):
+        self.performance_data[precommit.benchmark_id] = [
+            precommit.settings.challenge_id,
+            precommit.settings.difficulty,
+            precommit.details.num_nonces,
+            int(time.time() * 1000),
+            None
+        ]
+
+    async def on_benchmark_ready(self, benchmark_id: str, **kwargs):
+        if benchmark_id in self.performance_data:
+            self.performance_data[benchmark_id][-1] = int(time.time() * 1000)
+
     async def on_difficulty_samples(self, challenge_id: str, samples: list, **kwargs):
         self.difficulty_samples[challenge_id] = samples
 
     async def on_update(self):
-        if self.lock or self.num_precommits_submitted >= self.config.max_precommits_per_block:
+        if self.lock:
             return
+        if self.num_precommits_submitted >= self.config.max_precommits_per_block:
+            logger.info(f"reached max precommits per block: {self.config.max_precommits_per_block}")
+            return
+        if self.num_unresolved_precommits >= self.config.max_unresolved_precommits:
+            logger.info(f"reached max unresolved precommits: {self.config.max_precommits_per_block}")
+            return
+        now = int(time.time() * 1000)
+        prune_benchmark_ids = [
+            benchmark_id
+            for benchmark_id, data in self.performance_data.items()
+            if now - data[3] >= 3600000 # 1 hr
+        ]
+        for benchmark_id in prune_benchmark_ids:
+            self.performance_data.pop(benchmark_id, None)
         algo_selection = [
             (c_name, selection)
             for c_name, selection in self.config.algo_selection.items()
@@ -78,7 +120,24 @@ class Extension:
         c_id = self.challenge_name_2_id[c_name]
         a_name = selection[1].algorithm
         a_id = self.algorithm_name_2_id[f"{c_name}_{a_name}"]
-        num_nonces = selection[1].num_nonces
+
+        historic_data = [
+            data
+            for data in self.performance_data.values()
+            if data[0] == c_id and data[4] is not None
+        ]
+        if len(historic_data) == 0:
+            logger.info(f"insufficient historic data for challenge {c_name}. defaulting to initial num nonces {selection[1].initial_num_nonces}")
+            num_nonces = selection[1].initial_num_nonces
+        else:
+            average_duration_per_nonce = sum([
+                (data[4] - data[3]) / data[2]
+                for data in historic_data
+            ]) / len(historic_data)
+            target_num_nonces = selection[1].target_duration / average_duration_per_nonce
+            logger.info(f"average duration per nonce for challenge {c_name}: {average_duration_per_nonce}ms, target num nonces: {target_num_nonces}")
+            num_nonces = min(int(target_num_nonces), selection[1].max_num_nonces)
+
         difficulty_samples = self.difficulty_samples.get(c_id, [])
         if len(difficulty_samples) == 0:
             return
