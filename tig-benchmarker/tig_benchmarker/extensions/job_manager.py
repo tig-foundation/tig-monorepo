ecadd7a5ef2d1ee6eb07303767b2fa8189bdef69
FiveMovesAhead <ying@tig.foundation>
2024-10-13 19:04:56 +0100

    Patch issue where batch_merkle_root may be none.

--- tig-benchmarker/tig_benchmarker/extensions/job_manager.py
+++ tig-benchmarker/tig_benchmarker/extensions/job_manager.py
@@ -152,7 +152,7 @@ class JobManager:
         
         for job in self.jobs:
             if (
-                job.merkle_root is None or
+                any(x is None for x in job.batch_merkle_roots) or
                 len(job.sampled_nonces) == 0 or # benchmark not confirmed
                 len(job.merkle_proofs) == len(job.sampled_nonces) # already processed
             ):
