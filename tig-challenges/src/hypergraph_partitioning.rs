0c257de4f0601251dfd60df012e3c21cc394de23
germ3n <gasfggit@proton.me>
2024-10-22 19:00:04 +0200

    partially implement verify logic for hypergraph partitioning

--- tig-challenges/src/hypergraph_partitioning.rs
+++ tig-challenges/src/hypergraph_partitioning.rs
@@ -21,23 +21,25 @@ pub struct Difficulty
     pub num_vertices:                               usize,
     pub num_nodes:                                  usize,
     pub num_edges:                                  usize,
+    pub num_blocks:                                 usize,
 }
 
-impl crate::DifficultyTrait<3> for Difficulty 
+impl crate::DifficultyTrait<4> for Difficulty 
 {
-    fn from_arr(arr: &[i32; 3])                     -> Self 
+    fn from_arr(arr: &[i32; 4])                     -> Self 
     {
         return Self 
         {
             num_vertices:                           arr[0] as usize,
             num_nodes:                              arr[1] as usize,
             num_edges:                              arr[2] as usize,
+            num_blocks:                             arr[3] as usize
         };
     }
 
-    fn to_arr(&self)                                -> [i32; 3] 
+    fn to_arr(&self)                                -> [i32; 4] 
     {
-        return [ self.num_vertices as i32, self.num_nodes as i32, self.num_edges as i32 ];
+        return [ self.num_vertices as i32, self.num_nodes as i32, self.num_edges as i32, self.num_blocks as i32 ];
     }
 }
 
@@ -109,7 +111,7 @@ fn get_init_values(
     return (vertices, hyperedge_indices, hyperedges);
 }
 
-impl crate::ChallengeTrait<Solution, Difficulty, 3> for Challenge 
+impl crate::ChallengeTrait<Solution, Difficulty, 4> for Challenge 
 {
     #[cfg(feature = "cuda")]
     fn cuda_generate_instance(
@@ -142,8 +144,83 @@ impl crate::ChallengeTrait<Solution, Difficulty, 3> for Challenge
         });
     }
 
-    fn verify_solution(&self, solution: &Solution) -> Result<()> 
+    fn verify_solution(&self, solution: &Solution)  -> Result<()> 
     {
         return Ok(());
     }
+}
+
+fn recursive_bipartition(
+    vertices_subset:                    &Vec<u64>, 
+    partitions:                         &mut Vec<i32>,
+    current_id:                         i32, 
+    current_depth:                      u32, 
+    partitions_per_subset:              usize
+)
+{
+    if current_depth == 0
+    {
+        for i in 0..vertices_subset.len()
+        {
+            partitions[i]               = current_id;
+        }
+
+        return;
+    }
+
+    let half_partitions                 = partitions_per_subset / 2;
+    let left_partitions                 = half_partitions;
+    let right_partitions                = partitions_per_subset - half_partitions;
+
+    let target_left                     = vertices_subset.len() * left_partitions / partitions_per_subset;
+    let target_right                    = vertices_subset.len() - target_left;
+
+    let (left, right)                   = bipartition(vertices_subset, target_left, target_right);
+
+    recursive_bipartition(&left, partitions, current_id * 2, current_depth - 1, left_partitions);
+    recursive_bipartition(&right, partitions, current_id * 2 + 1, current_depth - 1, right_partitions);
+}
+
+fn bipartition(
+    vertices_subset:                    &Vec<u64>, 
+    target_left:                        usize, 
+    target_right:                       usize
+)                                                   -> (Vec<u64>, Vec<u64>)
+{
+    assert!(target_left + target_right == vertices_subset.len());
+
+    return (Vec::new(), Vec::new());
+}
+
+fn solve_shape(
+    hyperedges:                         &Vec<Vec<u64>>
+)                                                   -> (usize, usize)
+{
+    return (hyperedges.len(), hyperedges[0].len());
+}
+
+fn solve_greedy_bipartition(
+    vertices:                           &Vec<u64>, 
+    hyperedges:                         &Vec<Vec<u64>>,
+    num_partitions:                     Option<usize>
+)                                                   -> Vec<i32>
+{
+    let depth                           = num_partitions.unwrap_or(16).ilog2();
+    let M                               = solve_shape(hyperedges).0;
+
+    // Preprocessing: Build mappings
+    // vertex_to_hyperedges[v] will contain the hyperedge indices that include vertex v
+    let mut vertex_to_hyperedges:       Vec<Vec<usize>> = Vec::with_capacity(vertices.len());
+    for i in 0..hyperedges.len()
+    {
+        for j in hyperedges[i].iter()
+        {
+            vertex_to_hyperedges[*j as usize].push(i);
+        }
+    }
+
+    let mut partitions                  = vec![-1 as i32; vertices.len()];
+    recursive_bipartition(vertices, &mut partitions, 0, depth, num_partitions.unwrap_or(16));
+
+    return partitions;
 }
