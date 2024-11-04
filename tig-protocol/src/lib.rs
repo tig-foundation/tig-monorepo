561da5485187d08223b6437e931976988d72194d
germ3n <gasfggit@proton.me>
2024-11-04 15:34:12 +0100

    remove unused code from submit_precommit.rs reimplement submit_precommit in lib.rs to use the contract interfaces

--- tig-protocol/src/lib.rs
+++ tig-protocol/src/lib.rs
@@ -42,15 +42,6 @@ impl<'a, T: Context> Protocol<T> {
         submit_algorithm::execute(&self.ctx, player, details, code).await
     }
 
-    pub async fn submit_precommit(
-        &self,
-        player: &Player,
-        settings: BenchmarkSettings,
-        num_nonces: u32,
-    ) -> ProtocolResult<String> {
-        submit_precommit::execute(&self.ctx, player, settings, num_nonces).await
-    } 
-
     pub async fn submit_benchmark(
         &self,
         player: &Player,
@@ -89,20 +80,41 @@ impl<'a, T: Context> Protocol<T> {
         add_block::execute(&self.ctx).await
     }
 
-    pub async fn execute_benchmark(&self, player: &Player, settings: BenchmarkSettings, num_nonces: u32) -> ProtocolResult<String> 
+
+    pub async fn submit_precommit(
+        &self,
+        player:                     &Player,
+        settings:                   BenchmarkSettings,
+        num_nonces:                 u32,
+    )                                       -> ProtocolResult<String> 
     {
         //self.benchmarks.execute(player, &settings, num_nonces).await
 
         self.benchmarks.verify_player_owns_benchmark(player, &settings)?;
         self.benchmarks.verify_num_nonces(num_nonces)?;
-        let block = submit_precommit::get_block_by_id(&self.ctx, &settings.block_id).await?;
+        
+        let block                               = submit_precommit::get_block_by_id(&self.ctx, &settings.block_id).await?;
         self.benchmarks.verify_sufficient_lifespan(&self.ctx, &block).await?;
-        //let challenge = get_challenge_by_id(ctx, &settings.challenge_id, &block).await?;
-        //self.algorithms.verify_algorithm(ctx, &settings.algorithm_id, &block).await?;
-        //self.benchmarks.verify_benchmark_settings_are_unique(ctx, &settings).await?;
-        //self.benchmarks.verify_benchmark_difficulty(&settings.difficulty, &challenge, &block)?;
-        //let fee_paid = self.benchmarks.get_fee_paid(&player, num_nonces, &challenge)?;
+        
+        let challenge                           = self.challenges.get_challenge_by_id(&self.ctx, &settings.challenge_id, &block).await?;
+        self.algorithms.verify_algorithm(&self.ctx, &settings.algorithm_id, &block).await?;
+        self.benchmarks.verify_benchmark_settings_are_unique(&self.ctx, &settings).await?;
+        self.benchmarks.verify_benchmark_difficulty(&settings.difficulty, &challenge, &block)?;
 
-        Ok(String::from(""))
+        let fee_paid                            = submit_precommit::get_fee_paid(&player, num_nonces, &challenge)?;
+        let benchmark_id                        = self.ctx
+        .add_precommit_to_mempool(
+            settings,
+            PrecommitDetails 
+            {
+                block_started                   : block.details.height,
+                num_nonces                      : Some(num_nonces),
+                fee_paid                        : Some(fee_paid),
+            },
+        )
+        .await
+        .unwrap_or_else(|e| panic!("add_precommit_to_mempool error: {:?}", e));
+
+        return Ok(benchmark_id);
     }
 }
