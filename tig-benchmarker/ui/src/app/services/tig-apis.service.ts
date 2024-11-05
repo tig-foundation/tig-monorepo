7b010dba1b0d4153e3cfc1f4c957303300ae50de
Chadizzle <blanchard@live.co.za>
2024-11-05 11:47:33 +0200

    Updates UI

--- tig-benchmarker/ui/src/app/services/tig-apis.service.ts
+++ tig-benchmarker/ui/src/app/services/tig-apis.service.ts
@@ -1,18 +1,23 @@
 import axios from 'axios';
 import BigNumber from 'bignumber.js';
 import { inject, Injectable, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TigApisService {
-  player_stats : any = signal(null);
-  algorithms : any = signal(null);
-  challenges : any = signal(null);
-  price_info : any = signal(null);
+  player_stats: any = signal(null);
+  ready: any = signal(false);
+  algorithms: any = signal(null);
+  challenges: any = signal(null);
+  batches: any = signal(null);
+  price_info: any = signal(null);
+  price_info$ = toObservable(this.price_info);
   round = 44;
   player_id = signal('0x81a8ed48a188853442e3ff2b5eab0c9fa9a3c626');
   latest_block: any = signal(null);
+  latest_block$ = toObservable(this.latest_block);
   constructor() {
     this.init();
   }
@@ -29,10 +34,8 @@ export class TigApisService {
     const url =
       'https://mainnet-api.tig.foundation/get-block?round=42&include_data=true';
     const result = (await axios.get(url)).data;
-    console.log('getLatestBlock', result);
     if (result?.block) {
       this.latest_block.set(result.block);
-      console.log('latest_block', this.latest_block());
     }
   }
   async getAlgorithms() {
@@ -52,30 +55,39 @@ export class TigApisService {
             };
           })
         );
-        console.log('algorithms', this.algorithms());
+        this.checkReady();
       }
     }
   }
   async getChallenges() {
     console.log('getChallenges');
     if (this.latest_block()) {
-      const url = `https://mainnet-api.tig.foundation/get-challenges?block_id=${
-        this.latest_block()?.id
-      }`;
-      const result = (await axios.get(url)).data;
-      console.log('getChallenges', result);
-      // if (result?.algorithms) {
-      //   this.algorithms.set(
-      //     result.algorithms.map((a: any) => {
-      //       return {
-      //         id: a.id,
-      //         ...a.details,
-      //         state: a.state,
-      //         block_data: a.block_data,
-      //       };
-      //     })
-      //   );
-      // }
+      // const url = `https://mainnet-api.tig.foundation/get-challenges?block_id=${
+      //   this.latest_block()?.id
+      // }`;
+      // const result = (await axios.get(url)).data;
+      // console.log('getChallenges', result);
+      const temp_challenges = [
+        {
+          id: 'c001',
+          name: 'satisfiability',
+        },
+        {
+          id: 'c002',
+          name: 'vehicle_routing',
+        },
+        {
+          id: 'c003',
+          name: 'knapsack',
+        },
+        {
+          id: 'c004',
+          name: 'vector_search',
+        },
+      ];
+      console.log('challenges', temp_challenges);
+      this.challenges.set(temp_challenges);
+      this.checkReady();
     }
   }
 
@@ -115,7 +127,7 @@ export class TigApisService {
           .dividedBy(new BigNumber(Math.pow(10, 18)))
           .toString();
         this.player_stats.set(data);
-        console.log('player_stats', this.player_stats());
+        this.checkReady();
       }
     }
   }
@@ -128,10 +140,16 @@ export class TigApisService {
       const tig_data = result.pairs.filter(
         (p: any) => p.chainId === 'base' || p.baseToken.symbol === 'TIG'
       )[0];
-      console.log('price_info', tig_data);
       if (tig_data) {
         this.price_info.set(tig_data);
+        this.checkReady();
       }
+    }
+  }
+
+  checkReady() {
+    if (this.algorithms() && this.challenges() && this.player_stats()) {
+      this.ready.set(true);
     }
   }
 }
