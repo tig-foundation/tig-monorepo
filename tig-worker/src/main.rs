34317a8c774cedcaffe9579a3bd9e2a030499bb2
FiveMovesAhead <ying@tig.foundation>
2025-04-30 14:15:27 +0100

    Update tig-worker to invoke tig-runtime.

--- tig-worker/src/main.rs
+++ tig-worker/src/main.rs
@@ -1,199 +1,89 @@
-mod worker;
 use anyhow::{anyhow, Result};
 use clap::{arg, Command};
 use futures::stream::{self, StreamExt};
 use serde_json::json;
-use std::{fs, path::PathBuf, sync::Arc};
+use std::{fs, path::PathBuf};
+use tempfile::NamedTempFile;
 use tig_structs::core::*;
-use tig_utils::{compress_obj, dejsonify, jsonify, MerkleHash, MerkleTree};
-use tig_worker::OutputData;
+use tig_utils::{compress_obj, decompress_obj, dejsonify, jsonify, MerkleHash, MerkleTree};
 use tokio::runtime::Runtime;
 
 fn cli() -> Command {
     Command::new("tig-worker")
-        .about("Computes or verifies solutions")
+        .about("Computes batch of nonces and generates Merkle proofs")
         .arg_required_else_help(true)
-        .subcommand(
-            Command::new("compute_solution")
-                .about("Computes a solution")
-                .arg(
-                    arg!(<SETTINGS> "Settings json string or path to json file")
-                        .value_parser(clap::value_parser!(String)),
-                )
-                .arg(
-                    arg!(<RAND_HASH> "A string used in seed generation")
-                        .value_parser(clap::value_parser!(String)),
-                )
-                .arg(arg!(<NONCE> "Nonce value").value_parser(clap::value_parser!(u64)))
-                .arg(arg!(<WASM> "Path to a wasm file").value_parser(clap::value_parser!(PathBuf)))
-                .arg(
-                    arg!(--fuel [FUEL] "Optional maximum fuel parameter for WASM VM")
-                        .default_value("10000000000")
-                        .value_parser(clap::value_parser!(u64)),
-                )
-                .arg(
-                    arg!(--interval [INTERVAL] "Optional amount of fuel between signatures")
-                        .default_value("200000000")
-                        .value_parser(clap::value_parser!(u64)),
-                )
-                .arg(
-                    arg!(--mem [MEM] "Optional maximum memory parameter for WASM VM")
-                        .default_value("1000000000")
-                        .value_parser(clap::value_parser!(u64)),
-                ),
+        .arg(arg!(<RUNTIME> "Path to tig-runtime executable").value_parser(clap::value_parser!(PathBuf)))
+        .arg(
+            arg!(<SETTINGS> "Settings json string or path to json file")
+                .value_parser(clap::value_parser!(String)),
         )
-        .subcommand(
-            Command::new("verify_solution")
-                .about("Verifies a solution")
-                .arg(
-                    arg!(<SETTINGS> "Settings json string or path to json file")
-                        .value_parser(clap::value_parser!(String)),
-                )
-                .arg(
-                    arg!(<RAND_HASH> "A string used in seed generation")
-                        .value_parser(clap::value_parser!(String)),
-                )
-                .arg(arg!(<NONCE> "Nonce value").value_parser(clap::value_parser!(u64)))
-                .arg(
-                    arg!(<SOLUTION> "Solution json string or path to json file")
-                        .value_parser(clap::value_parser!(String)),
-                ),
+        .arg(
+            arg!(<RAND_HASH> "A string used in seed generation")
+                .value_parser(clap::value_parser!(String)),
         )
-        .subcommand(
-            Command::new("compute_batch")
-                .about("Computes batch of nonces and generates Merkle proofs")
-                .arg(
-                    arg!(<SETTINGS> "Settings json string or path to json file")
-                        .value_parser(clap::value_parser!(String)),
-                )
-                .arg(
-                    arg!(<RAND_HASH> "A string used in seed generation")
-                        .value_parser(clap::value_parser!(String)),
-                )
-                .arg(arg!(<START_NONCE> "Starting nonce").value_parser(clap::value_parser!(u64)))
-                .arg(
-                    arg!(<NUM_NONCES> "Number of nonces to compute")
-                        .value_parser(clap::value_parser!(u64)),
-                )
-                .arg(
-                    arg!(<BATCH_SIZE> "Batch size for Merkle tree")
-                        .value_parser(clap::value_parser!(u64)),
-                )
-                .arg(arg!(<WASM> "Path to a wasm file").value_parser(clap::value_parser!(PathBuf)))
-                .arg(
-                    arg!(--fuel [FUEL] "Optional maximum fuel parameter for WASM VM")
-                        .default_value("2000000000")
-                        .value_parser(clap::value_parser!(u64)),
-                )
-                .arg(
-                    arg!(--mem [MEM] "Optional maximum memory parameter for WASM VM")
-                        .default_value("1000000000")
-                        .value_parser(clap::value_parser!(u64)),
-                )
-                .arg(
-                    arg!(--workers [WORKERS] "Number of worker threads")
-                        .default_value("1")
-                        .value_parser(clap::value_parser!(usize)),
-                )
-                .arg(
-                    arg!(--output [OUTPUT_FOLDER] "If set, the data for nonce will be saved as '<nonce>.json' in this folder")
-                        .value_parser(clap::value_parser!(PathBuf)),
-                ),
+        .arg(arg!(<START_NONCE> "Starting nonce").value_parser(clap::value_parser!(u64)))
+        .arg(
+            arg!(<NUM_NONCES> "Number of nonces to compute")
+                .value_parser(clap::value_parser!(u64)),
+        )
+        .arg(
+            arg!(<BATCH_SIZE> "Batch size for Merkle tree")
+                .value_parser(clap::value_parser!(u64)),
+        )
+        .arg(
+            arg!(<BINARY> "Path to a shared object (*.so) file")
+                .value_parser(clap::value_parser!(PathBuf)),
+        )
+        .arg(
+            arg!(--ptx [PTX] "Path to a CUDA ptx file")
+                .value_parser(clap::value_parser!(PathBuf)),
+        )
+        .arg(
+            arg!(--fuel [FUEL] "Optional maximum fuel parameter for runtime")
+                .default_value("2000000000")
+                .value_parser(clap::value_parser!(u64)),
+        )
+        .arg(
+            arg!(--workers [WORKERS] "Number of worker threads")
+                .default_value("1")
+                .value_parser(clap::value_parser!(usize)),
+        )
+        .arg(
+            arg!(--output [OUTPUT_FOLDER] "If set, the data for nonce will be saved as '<nonce>.json' in this folder")
+                .value_parser(clap::value_parser!(PathBuf)),
         )
 }
 
 fn main() {
     let matches = cli().get_matches();
 
-    if let Err(e) = match matches.subcommand() {
-        Some(("compute_solution", sub_m)) => compute_solution(
-            sub_m.get_one::<String>("SETTINGS").unwrap().clone(),
-            sub_m.get_one::<String>("RAND_HASH").unwrap().clone(),
-            *sub_m.get_one::<u64>("NONCE").unwrap(),
-            sub_m.get_one::<PathBuf>("WASM").unwrap().clone(),
-            *sub_m.get_one::<u64>("mem").unwrap(),
-            *sub_m.get_one::<u64>("fuel").unwrap(),
-        ),
-        Some(("verify_solution", sub_m)) => verify_solution(
-            sub_m.get_one::<String>("SETTINGS").unwrap().clone(),
-            sub_m.get_one::<String>("RAND_HASH").unwrap().clone(),
-            *sub_m.get_one::<u64>("NONCE").unwrap(),
-            sub_m.get_one::<String>("SOLUTION").unwrap().clone(),
-        ),
-        Some(("compute_batch", sub_m)) => compute_batch(
-            sub_m.get_one::<String>("SETTINGS").unwrap().clone(),
-            sub_m.get_one::<String>("RAND_HASH").unwrap().clone(),
-            *sub_m.get_one::<u64>("START_NONCE").unwrap(),
-            *sub_m.get_one::<u64>("NUM_NONCES").unwrap(),
-            *sub_m.get_one::<u64>("BATCH_SIZE").unwrap(),
-            sub_m.get_one::<PathBuf>("WASM").unwrap().clone(),
-            *sub_m.get_one::<u64>("mem").unwrap(),
-            *sub_m.get_one::<u64>("fuel").unwrap(),
-            *sub_m.get_one::<usize>("workers").unwrap(),
-            sub_m.get_one::<PathBuf>("output").cloned(),
-        ),
-        _ => Err(anyhow!("Invalid subcommand")),
-    } {
+    if let Err(e) = compute_batch(
+        matches.get_one::<PathBuf>("RUNTIME").unwrap().clone(),
+        matches.get_one::<String>("SETTINGS").unwrap().clone(),
+        matches.get_one::<String>("RAND_HASH").unwrap().clone(),
+        *matches.get_one::<u64>("START_NONCE").unwrap(),
+        *matches.get_one::<u64>("NUM_NONCES").unwrap(),
+        *matches.get_one::<u64>("BATCH_SIZE").unwrap(),
+        matches.get_one::<PathBuf>("BINARY").unwrap().clone(),
+        matches.get_one::<PathBuf>("ptx").cloned(),
+        *matches.get_one::<u64>("fuel").unwrap(),
+        *matches.get_one::<usize>("workers").unwrap(),
+        matches.get_one::<PathBuf>("output").cloned(),
+    ) {
         eprintln!("Error: {}", e);
         std::process::exit(1);
     }
 }
 
-fn compute_solution(
-    settings: String,
-    rand_hash: String,
-    nonce: u64,
-    wasm_path: PathBuf,
-    max_memory: u64,
-    max_fuel: u64,
-) -> Result<()> {
-    let settings = load_settings(&settings);
-    let wasm = load_wasm(&wasm_path);
-
-    let (output_data, err_msg) = worker::compute_solution(
-        &settings,
-        &rand_hash,
-        nonce,
-        wasm.as_slice(),
-        max_memory,
-        max_fuel,
-    )?;
-    println!("{}", jsonify(&output_data));
-    if let Some(err_msg) = err_msg {
-        return Err(anyhow!("Runtime error: {}", err_msg));
-    } else if output_data.solution.len() == 0 {
-        return Err(anyhow!("No solution found"));
-    }
-    worker::verify_solution(&settings, &rand_hash, nonce, &output_data.solution)
-        .map_err(|e| anyhow!("Invalid solution: {}", e))
-}
-
-fn verify_solution(
-    settings: String,
-    rand_hash: String,
-    nonce: u64,
-    solution: String,
-) -> Result<()> {
-    let settings = load_settings(&settings);
-    let solution = load_solution(&solution);
-
-    match worker::verify_solution(&settings, &rand_hash, nonce, &solution) {
-        Ok(()) => {
-            println!("Solution is valid");
-            Ok(())
-        }
-        Err(e) => Err(anyhow!("Invalid solution: {}", e)),
-    }
-}
-
 fn compute_batch(
+    runtime_path: PathBuf,
     settings: String,
     rand_hash: String,
     start_nonce: u64,
     num_nonces: u64,
     batch_size: u64,
-    wasm_path: PathBuf,
-    max_memory: u64,
+    binary_path: PathBuf,
+    ptx_path: Option<PathBuf>,
     max_fuel: u64,
     num_workers: usize,
     output_folder: Option<PathBuf>,
@@ -211,8 +101,30 @@ fn compute_batch(
         fs::create_dir_all(path)?;
     }
 
-    let settings = Arc::new(load_settings(&settings));
-    let wasm = Arc::new(load_wasm(&wasm_path));
+    let settings = load_settings(&settings);
+    match settings.challenge_id.as_str() {
+        "c004" => {
+            #[cfg(not(feature = "cuda"))]
+            panic!("tig-worker was not compiled with '--features cuda'");
+
+            #[cfg(feature = "cuda")]
+            if ptx_path.is_none() {
+                return Err(anyhow!(
+                    "PTX file is required for challenge {}",
+                    settings.challenge_id
+                ));
+            }
+        }
+        _ => {
+            if ptx_path.is_some() {
+                return Err(anyhow!(
+                    "PTX file is not required for challenge {}",
+                    settings.challenge_id
+                ));
+            }
+        }
+    }
+    let settings = jsonify(&settings);
 
     let runtime = Runtime::new()?;
 
@@ -223,36 +135,86 @@ fn compute_batch(
         // Create a stream of nonces and process them concurrently
         let results = stream::iter(start_nonce..(start_nonce + num_nonces))
             .map(|nonce| {
-                let settings = Arc::clone(&settings);
-                let wasm = Arc::clone(&wasm);
+                let runtime_path = runtime_path.clone();
+                let settings = settings.clone();
                 let rand_hash = rand_hash.clone();
+                let binary_path = binary_path.clone();
                 let output_folder = output_folder.clone();
+                #[cfg(feature = "cuda")]
+                let ptx_path = ptx_path.clone();
+                #[cfg(feature = "cuda")]
+                let num_gpus = cudarc::runtime::result::device::get_count().unwrap() as u64;
 
                 tokio::spawn(async move {
-                    let (output_data, err_msg) = worker::compute_solution(
-                        &settings,
-                        &rand_hash,
-                        nonce,
-                        wasm.as_slice(),
-                        max_memory,
-                        max_fuel,
-                    )?;
-                    let is_solution = err_msg.is_none()
-                        && worker::verify_solution(
-                            &settings,
-                            &rand_hash,
-                            nonce,
-                            &output_data.solution,
-                        )
-                        .is_ok();
-                    let hash = MerkleHash::from(output_data.clone());
+                    let temp_file = NamedTempFile::new()?;
+                    let mut cmd = std::process::Command::new(runtime_path);
+                    cmd.arg("compute_solution")
+                        .arg(settings)
+                        .arg(rand_hash)
+                        .arg(nonce.to_string())
+                        .arg(binary_path)
+                        .arg("--output")
+                        .arg(temp_file.path())
+                        .arg("--compress");
 
-                    Ok::<(u64, MerkleHash, bool, Option<OutputData>), anyhow::Error>((
-                        nonce,
-                        hash,
-                        is_solution,
-                        output_folder.is_some().then(|| output_data),
-                    ))
+                    #[cfg(feature = "cuda")]
+                    if let Some(ptx_path) = ptx_path {
+                        cmd.arg("--ptx")
+                            .arg(ptx_path)
+                            .arg("--gpu")
+                            .arg((nonce % num_gpus).to_string());
+                    }
+
+                    let output = cmd.output().unwrap();
+
+                    let exit_code = output.status.code();
+                    let is_solution = output.status.success();
+                    if exit_code == Some(87) {
+                        // out of fuel
+                        let mut runtime_signature = 0;
+                        let stdout = String::from_utf8_lossy(&output.stdout);
+                        let mut lines = stdout.lines().rev();
+                        while let Some(line) = lines.next() {
+                            if line.starts_with("Runtime signature: ") {
+                                if let Some(sig) = line.strip_prefix("Runtime signature: ") {
+                                    if let Ok(sig) = sig.trim().parse::<u64>() {
+                                        runtime_signature = sig;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+
+                        let output_data = OutputData {
+                            nonce,
+                            solution: Solution::new(),
+                            fuel_consumed: max_fuel,
+                            runtime_signature,
+                        };
+                        let hash = MerkleHash::from(output_data.clone());
+                        Ok::<(u64, MerkleHash, bool, Option<OutputData>), anyhow::Error>((
+                            nonce,
+                            hash,
+                            is_solution,
+                            output_folder.is_some().then(|| output_data),
+                        ))
+                    } else if is_solution || exit_code == Some(86) || exit_code == Some(85) {
+                        let bytes = fs::read(temp_file.path())?;
+                        let output_data: OutputData = decompress_obj(&bytes)?;
+                        let hash = MerkleHash::from(output_data.clone());
+                        Ok::<(u64, MerkleHash, bool, Option<OutputData>), anyhow::Error>((
+                            nonce,
+                            hash,
+                            is_solution,
+                            output_folder.is_some().then(|| output_data),
+                        ))
+                    } else {
+                        Err(anyhow!(
+                            "Failed to compute nonce {}: {:?}",
+                            nonce,
+                            output.status
+                        ))
+                    }
                 })
             })
             .buffer_unordered(num_workers)
@@ -301,29 +263,6 @@ fn load_settings(settings: &str) -> BenchmarkSettings {
 
     dejsonify::<BenchmarkSettings>(&settings).unwrap_or_else(|_| {
         eprintln!("Failed to parse settings");
-        std::process::exit(1);
-    })
-}
-
-fn load_solution(solution: &str) -> worker::Solution {
-    let solution = if solution.ends_with(".json") {
-        fs::read_to_string(&solution).unwrap_or_else(|_| {
-            eprintln!("Failed to read solution file: {}", solution);
-            std::process::exit(1);
-        })
-    } else {
-        solution.to_string()
-    };
-
-    dejsonify::<worker::Solution>(&solution).unwrap_or_else(|_| {
-        eprintln!("Failed to parse solution");
-        std::process::exit(1);
-    })
-}
-
-fn load_wasm(wasm_path: &PathBuf) -> Vec<u8> {
-    fs::read(wasm_path).unwrap_or_else(|_| {
-        eprintln!("Failed to read wasm file: {}", wasm_path.display());
         std::process::exit(1);
     })
 }
