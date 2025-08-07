5255fedf66dc4dc1e23fca90f741e4a7197caf28
germ3n <gasfggit@proton.me>
2025-08-07 13:35:07 +0100

    add main

---
+++ tig-binary/src/main.rs
@@ -0,0 +1,10 @@
+extern crate tig_algorithms;
+extern crate tig_challenges;
+
+#[cfg(feature = "entry_point")]
+mod entry_point;
+
+#[cfg(feature = "entry_point")]
+fn main() {
+    println!("Hello, world!");
+}
