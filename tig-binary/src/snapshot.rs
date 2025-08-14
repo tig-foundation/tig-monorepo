4218dabfc4d27b7392d20646fcaaef27b2327669
germ3n <gasfggit@proton.me>
2025-08-14 15:34:14 +0100

    fix snapshot

--- tig-binary/src/snapshot.rs
+++ tig-binary/src/snapshot.rs
@@ -184,7 +184,7 @@ impl DeltaSnapshot {
 
         for i in 0..31 {
             if old.registers.gprs[i] != new.registers.gprs[i] {
-                delta.changed_regs.push(Registers::X(i));
+                delta.changed_regs.push(Registers::X(i as u8, new.registers.gprs[i]));
             }
         }
 
@@ -226,13 +226,13 @@ impl DeltaSnapshot {
 
         for i in 0..32 {
             if old.registers.vregs[i] != new.registers.vregs[i] {
-                delta.changed_regs.push(Registers::V(i));
+                delta.changed_regs.push(Registers::V(i as u8, new.registers.vregs[i]));
             }
         }
 
         for i in 0..16 {
             if old.registers.predicates[i] != new.registers.predicates[i] {
-                delta.changed_regs.push(Registers::P(i));
+                delta.changed_regs.push(Registers::P(i as u8, new.registers.predicates[i]));
             }
         }
 
@@ -252,37 +252,7 @@ impl DeltaSnapshot {
 #[repr(u8)]
 #[derive(Debug)]
 pub enum Registers {
-    X0(u64) = 0,
-    X1(u64) = 1,
-    X2(u64) = 2,
-    X3(u64) = 3,
-    X4(u64) = 4,
-    X5(u64) = 5,
-    X6(u64) = 6,
-    X7(u64) = 7,
-    X8(u64) = 8,
-    X9(u64) = 9,
-    X10(u64) = 10,
-    X11(u64) = 11,
-    X12(u64) = 12,
-    X13(u64) = 13,
-    X14(u64) = 14,
-    X15(u64) = 15,
-    X16(u64) = 16,
-    X17(u64) = 17,
-    X18(u64) = 18,
-    X19(u64) = 19,
-    X20(u64) = 20,
-    X21(u64) = 21,
-    X22(u64) = 22,
-    X23(u64) = 23,
-    X24(u64) = 24,
-    X25(u64) = 25,
-    X26(u64) = 26,
-    X27(u64) = 27,
-    X28(u64) = 28,
-    X29(u64) = 29,
-    X30(u64) = 30,
+    X(u8, u64) = 0, // x<n>, value
     SP(u64) = 31,
     LR(u64) = 32,
     PC(u64) = 33,
@@ -293,54 +263,8 @@ pub enum Registers {
     TPIDRRO_EL0(u64) = 38,
     CNTVCT_EL0(u64) = 39,
     CNTFRQ_EL0(u64) = 40,
-    V0(u128) = 41,
-    V1(u128) = 42,
-    V2(u128) = 43,
-    V3(u128) = 44,
-    V4(u128) = 45,
-    V5(u128) = 46,
-    V6(u128) = 47,
-    V7(u128) = 48,
-    V8(u128) = 49,
-    V9(u128) = 50,
-    V10(u128) = 51,
-    V11(u128) = 52,
-    V12(u128) = 53,
-    V13(u128) = 54,
-    V14(u128) = 55,
-    V15(u128) = 56,
-    V16(u128) = 57,
-    V17(u128) = 58,
-    V18(u128) = 59,
-    V19(u128) = 60,
-    V20(u128) = 61,
-    V21(u128) = 62,
-    V22(u128) = 63,
-    V23(u128) = 64,
-    V24(u128) = 65,
-    V25(u128) = 66,
-    V26(u128) = 67,
-    V27(u128) = 68,
-    V28(u128) = 69,
-    V29(u128) = 70,
-    V30(u128) = 71,
-    V31(u128) = 72,
-    P0(u128) = 73,
-    P1(u128) = 74,
-    P2(u128) = 75,
-    P3(u128) = 76,
-    P4(u128) = 77,
-    P5(u128) = 78,
-    P6(u128) = 79,
-    P7(u128) = 80,
-    P8(u128) = 81,
-    P9(u128) = 82,
-    P10(u128) = 83,
-    P11(u128) = 84,
-    P12(u128) = 85,
-    P13(u128) = 86,
-    P14(u128) = 87,
-    P15(u128) = 88,
+    V(u8, u128) = 41, // v<n>, value
+    P(u8, u128) = 73, // p<n>, value
     FFR(u128) = 89,
     VG(u32) = 90,
 }
