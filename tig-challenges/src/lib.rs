898d619514ab464081a03cd4f59c7ed100cb2c54
germ3n <gasfggit@proton.me>
2025-09-09 12:04:39 +0200

    set challenge id for zk to 007

--- tig-challenges/src/lib.rs
+++ tig-challenges/src/lib.rs
@@ -20,7 +20,7 @@ pub use vector_search as c004;
 pub mod hypergraph;
 #[cfg(feature = "c005")]
 pub use hypergraph as c005;
-#[cfg(feature = "c006")]
+#[cfg(feature = "c007")]
 pub mod zk;
-#[cfg(feature = "c006")]
-pub use zk as c006;
+#[cfg(feature = "c007")]
+pub use zk as c007;
