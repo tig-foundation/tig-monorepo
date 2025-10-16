e9463938f9953ef99fdba3a05bd32d181f128d97
FiveMovesAhead <ying@tig.foundation>
2025-10-16 11:10:26 +0100

    Submitted vector_search/optimal_ann

---
+++ tig-algorithms/src/vector_search/optimal_ann/kernels.cu
@@ -0,0 +1,19 @@
+/*!
+Copyright 2024 Just
+
+Licensed under the TIG Inbound Game License v1.0 or (at your option) any later
+version (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at
+
+https://github.com/tig-foundation/tig-monorepo/tree/main/docs/licenses
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+language governing permissions and limitations under the License.
+*/
+
+extern "C" __global__ void do_nothing()
+{
+    // This kernel does nothing
+}             
