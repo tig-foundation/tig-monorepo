f3590f886139b46b679e4ff0aa2798975747bfc9
FiveMovesAhead <ying@tig.foundation>
2025-12-02 16:55:05 +0000

    Submitted vector_search/kdtree_truemean

---
+++ tig-algorithms/src/vector_search/kdtree_truemean/kernels.cu
@@ -0,0 +1,23 @@
+/*!
+Copyright 2025 Dan
+
+Identity of Submitter Dan
+
+UAI null
+
+Licensed under the TIG Inbound Game License v2.0 or (at your option) any later
+version (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at
+
+https://github.com/tig-foundation/tig-monorepo/tree/main/docs/licenses
+
+Unless required by applicable law or agreed to in writing, software distributed
+under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+CONDITIONS OF ANY KIND, either express or implied. See the License for the specific
+language governing permissions and limitations under the License.
+*/
+
+extern "C" __global__ void do_nothing()
+{
+    // This kernel does nothing
+}             
