dce77a8e6a1ead7e52330a13d624af8fe50dc125
0x1884f0fe1208f38a1433b706790e22be9fed1f7c <contact@tig.foundation>
2026-02-18 20:06:33 +0000

    "Player 0x1884f0fe1208f38a1433b706790e22be9fed1f7c submitted code job_scheduling/adaptive_js_v2"

---
+++ tig-algorithms/src/job_scheduling/adaptive_js_v2/mod.rs
@@ -0,0 +1,12 @@
+pub mod types;
+pub mod preprocess;
+pub mod detect;
+pub mod infra;
+pub mod solver;
+pub mod flow_shop;
+pub mod hybrid_flow_shop;
+pub mod job_shop;
+pub mod fjsp_medium;
+pub mod fjsp_high;
+
+pub use solver::{solve_challenge, help};
